1f87b4c470c285a1faa2957111082b68e4d7ef83
denispodshivalov <denpodshivalov091@gmail.com>
2024-07-30 00:34:54 +1000

    entrypoint fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-while ! nc -z -w 1 $POSTGRES_HOST ${POSTGRES_PORT:-POSTGRES_PORT}; do
-  sleep 1
-done
-
-sleep 5
-
-
-python manage.py migrate
-
-python manage.py runserver 0.0.0.0:8000 --noreload
+poetry run python -Om src
